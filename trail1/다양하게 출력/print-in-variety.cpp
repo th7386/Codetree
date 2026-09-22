6d299d09eb3b43d80e840ba464ed25e5c444380a
TAEHYEONG GU <48504586+th7386@users.noreply.github.com>
2026-09-23 08:33:53 +0900

    [Passed] 다양하게 출력 (1ms, 0MB)

---
+++ trail1/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(10);
+    cout << "Total days in Year" << '\n' << 365 << '\n' << "Circumference rate" << '\n' << 3.1415926535;
+    return 0;
+}
